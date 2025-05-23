0fdaf0e12681bfa3fe2319349c6e18c0a0ead421
krafairus <64279814+krafairus@users.noreply.github.com>
2025-05-23 17:35:27 -0500

    Add files via upload
    welcome

---
+++ resources/zn_en.ts
@@ -0,0 +1,208 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en">
+<context>
+    <name>InstallerWorker</name>
+    <message>
+        <location filename="../zen-browser-start" line="166"/>
+        <source>Error al crear directorios: {0}</source>
+        <translation>Error creating directories: {0}</translation>
+    </message>
+    <message>
+        <location filename="../zen-browser-start" line="169"/>
+        <source>Verificando actualizaciones...</source>
+        <translation>Checking for updates...</translation>
+    </message>
+    <message>
+        <location filename="../zen-browser-start" line="187"/>
+        <source>Nueva versión: {0}</source>
+        <translation>New version: {0}</translation>
+    </message>
+    <message>
+        <location filename="../zen-browser-start" line="176"/>
+        <source>Actualizando de v{0}...</source>
+        <translation>Updating from v{0}...</translation>
+    </message>
+    <message>
+        <location filename="../zen-browser-start" line="179"/>
+        <source>Ya tienes la última versión (v{0})</source>
+        <translation>You already have the latest version (v{0})</translation>
+    </message>
+    <message>
+        <location filename="../zen-browser-start" line="182"/>
+        <source>Error al verificar actualizaciones: {0}</source>
+        <translation>Error checking for updates: {0}</translation>
+    </message>
+    <message>
+        <location filename="../zen-browser-start" line="188"/>
+        <source>Descargando Zen Browser...</source>
+        <translation>Downloading Zen Browser...</translation>
+    </message>
+    <message>
+        <location filename="../zen-browser-start" line="191"/>
+        <source>Error durante la instalación: {0}</source>
+        <translation>Installation error: {0}</translation>
+    </message>
+    <message>
+        <location filename="../zen-browser-start" line="209"/>
+        <source>No se encontró application.ini</source>
+        <translation>application.ini not found</translation>
+    </message>
+    <message>
+        <location filename="../zen-browser-start" line="215"/>
+        <source>No se encontró la versión</source>
+        <translation>Version not found</translation>
+    </message>
+    <message>
+        <location filename="../zen-browser-start" line="220"/>
+        <source>Error al extraer AppImage: {0}</source>
+        <translation>Error extracting AppImage: {0}</translation>
+    </message>
+    <message>
+        <location filename="../zen-browser-start" line="222"/>
+        <source>Error al obtener versión instalada: {0}</source>
+        <translation>Error getting installed version: {0}</translation>
+    </message>
+    <message>
+        <location filename="../zen-browser-start" line="239"/>
+        <source>No se encontró el AppImage en los releases</source>
+        <translation>AppImage not found in releases</translation>
+    </message>
+    <message>
+        <location filename="../zen-browser-start" line="241"/>
+        <source>Error al obtener última versión: {0}</source>
+        <translation>Error getting latest version: {0}</translation>
+    </message>
+    <message>
+        <location filename="../zen-browser-start" line="246"/>
+        <source>Descargando actualización... 0%</source>
+        <translation>Downloading update... 0%</translation>
+    </message>
+    <message>
+        <location filename="../zen-browser-start" line="259"/>
+        <source>Descargando actualización... {0}%</source>
+        <translation>Downloading update... {0}%</translation>
+    </message>
+    <message>
+        <location filename="../zen-browser-start" line="262"/>
+        <source>Archivo descargado vacío</source>
+        <translation>Downloaded file is empty</translation>
+    </message>
+    <message>
+        <location filename="../zen-browser-start" line="269"/>
+        <source>Instalando...</source>
+        <translation>Installing...</translation>
+    </message>
+    <message>
+        <location filename="../zen-browser-start" line="275"/>
+        <source>Error en descarga: {0}</source>
+        <translation>Download error: {0}</translation>
+    </message>
+    <message>
+        <location filename="../zen-browser-start" line="298"/>
+        <source>No se encontró zen.desktop en el AppImage</source>
+        <translation>zen.desktop not found in AppImage</translation>
+    </message>
+    <message>
+        <location filename="../zen-browser-start" line="300"/>
+        <source>No se encontró zen.png en el AppImage</source>
+        <translation>zen.png not found in AppImage</translation>
+    </message>
+    <message>
+        <location filename="../zen-browser-start" line="325"/>
+        <source>¡Instalación completada!</source>
+        <translation>Installation completed!</translation>
+    </message>
+    <message>
+        <location filename="../zen-browser-start" line="329"/>
+        <source>Error al procesar AppImage: {0}</source>
+        <translation>Error processing AppImage: {0}</translation>
+    </message>
+    <message>
+        <location filename="../zen-browser-start" line="350"/>
+        <source>Iniciando Zen Browser...</source>
+        <translation>Starting Zen Browser...</translation>
+    </message>
+    <message>
+        <location filename="../zen-browser-start" line="366"/>
+        <source>Aplicación iniciada</source>
+        <translation>Application started</translation>
+    </message>
+    <message>
+        <location filename="../zen-browser-start" line="368"/>
+        <source>Error al iniciar: {0}</source>
+        <translation>Startup error: {0}</translation>
+    </message>
+</context>
+<context>
+    <name>UninstallDialog</name>
+    <message>
+        <location filename="../zen-browser-start" line="60"/>
+        <source>Desinstalar Zen Browser</source>
+        <translation>Uninstall Zen Browser</translation>
+    </message>
+    <message>
+        <location filename="../zen-browser-start" line="66"/>
+        <source>¿Estás seguro que deseas desinstalar Zen Browser?</source>
+        <translation>Are you sure you want to uninstall Zen Browser?</translation>
+    </message>
+    <message>
+        <location filename="../zen-browser-start" line="71"/>
+        <source>Cancelar</source>
+        <translation>Cancel</translation>
+    </message>
+    <message>
+        <location filename="../zen-browser-start" line="72"/>
+        <source>Desinstalar</source>
+        <translation>Uninstall</translation>
+    </message>
+    <message>
+        <location filename="../zen-browser-start" line="118"/>
+        <source>Desinstalación completada</source>
+        <translation>Uninstallation completed</translation>
+    </message>
+    <message>
+        <location filename="../zen-browser-start" line="118"/>
+        <source>Zen Browser ha sido desinstalado correctamente.</source>
+        <translation>Zen Browser has been successfully uninstalled.</translation>
+    </message>
+    <message>
+        <location filename="../zen-browser-start" line="124"/>
+        <source>Error</source>
+        <translation>Error</translation>
+    </message>
+    <message>
+        <location filename="../zen-browser-start" line="124"/>
+        <source>No se pudo completar la desinstalación: {0}</source>
+        <translation>Could not complete uninstallation: {0}</translation>
+    </message>
+</context>
+<context>
+    <name>ZenInstaller</name>
+    <message>
+        <location filename="../zen-browser-start" line="385"/>
+        <source>Zen Browser</source>
+        <translation>Zen Browser</translation>
+    </message>
+    <message>
+        <location filename="../zen-browser-start" line="524"/>
+        <source>Actualizando</source>
+        <translation>Updating</translation>
+    </message>
+    <message>
+        <location filename="../zen-browser-start" line="524"/>
+        <source>Descargando</source>
+        <translation>Downloading</translation>
+    </message>
+    <message>
+        <location filename="../zen-browser-start" line="524"/>
+        <source>Iniciando</source>
+        <translation>Starting</translation>
+    </message>
+    <message>
+        <location filename="../zen-browser-start" line="533"/>
+        <source>Error: {0}</source>
+        <translation>Error: {0}</translation>
+    </message>
+</context>
+</TS>
---
+++ zen-browser-start.pro
@@ -0,0 +1,2 @@
+SOURCES = zen-browser-start
+TRANSLATIONS = resources/zn_en.ts resources/zn_pt.ts
